fee5cfaf442c7ceef4a2a848ea7efd373d2a0ad8
Emmanuel Lopez Guerrero <manelpz@icloud.com>
2026-09-08 21:24:42 -0600

    primer acercamiento crud

---
+++ crud_poc/src/Customer.java
@@ -0,0 +1,19 @@
+
+
+public class Customer {
+
+    private int _id;
+    private String _firstName;
+    private String _lastName;
+    private int age;
+
+    public Customer(int id, String firstName, String lastName, int age){
+        this._id = id;
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this._age = age; 
+
+    }
+
+
+}
